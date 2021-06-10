e84ccdc0c9afc024b4de9b9dfff9e8bde575be64
Andrew Ghostuhin <me@torinasakura.name>
2021-06-10 16:21:10 +0300

    Delete grpc-playground/applications/renderer/src/.next directory

--- grpc-playground/applications/renderer/src/.next/static/sbiQdpLgC9MXpHX1DLtPc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d71ddcf34a7779e298d3.js"],"/_error":["static/chunks/pages/_error-f51999d36b65f5a410ba.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
